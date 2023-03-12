5221e39f15ccd942b948f90cc067720fcf478aa0
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-12 15:56:10 -0400

    added surface area of a shpere calculator

--- SAsphere.cpp
+++ SAsphere.cpp
@@ -1,1 +1,25 @@
 // This file find the surface area of a sphere.
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+std::cout <<"\n****************Surface area of a sphere calculator****************\n";
+
+double radius;
+double Pi;
+double result;
+// 4 pi r squared
+
+std::cout <<"\nwhat is the radius of your sphere?\n";
+std::cout <<"Enter the radius: ";
+std::cin >> radius;
+
+result = 4 * 3.14 * radius * radius;
+
+ std::cout << "result: " << result << '\n';
+
+return 0;
+
+}
