b64d3dcc1840cb53b4ce46fe96c8c3a91a5ed9c6
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-17 13:27:03 -0400

    started SASquarePyramid calc

---
+++ SASquarePyramid.cpp
@@ -0,0 +1,16 @@
+// This file finds the Surface Area of a Square Pyramid.
+
+#include <iostream>
+#include <cmath>
+
+double result;
+double BaseEdge;
+double Height;
+double Slant;
+double BasePerimeter;
+
+
+int main(){
+
+    
+}
