aba27374552f4eff2276d4a22f47bf1e44d9bab8
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-18 11:40:40 -0400

    added things

--- SASquarePyramid.cpp
+++ SASquarePyramid.cpp
@@ -13,11 +13,11 @@ double BasePerimeter;
 int main(){
     std::cout <<"ENTER ALL IN CM!\n";
 
-    std::cout <<"Enter the base edge: ";
-    std::cin >> BaseEdge;
+    // std::cout <<"Enter the base edge: ";
+    // std::cin >> BaseEdge;
 
-    std::cout <<"Enter the Height: ";
-    std::cin >> Height;
+    // std::cout <<"Enter the Height: ";
+    // std::cin >> Height;
 
     std::cout <<"Enter the Base Perimeter: ";
     std::cin >> BasePerimeter;
@@ -26,7 +26,7 @@ int main(){
     std::cin >> Slant;
 
     result = BasePerimeter * Slant / 2;
-    std::cout <<"Result: " << result;
+    std::cout << "result: " << result << '\n';
 
     
 
