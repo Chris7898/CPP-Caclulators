8a6e8a82c3b24f692e26c6c0edc925ccce9bd114
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-20 18:39:36 -0400

    did some stuff

--- SAsphere.cpp
+++ SAsphere.cpp
@@ -8,15 +8,15 @@ int main(){
 std::cout <<"\n****************Surface area of a sphere calculator****************\n";
 
 double radius;
-double Pi;
+double Pi = 3.14;
 double result;
-// 4 pi r squared
+
 
 std::cout <<"\nwhat is the radius of your sphere?\n";
 std::cout <<"Enter the radius: ";
 std::cin >> radius;
 
-result = 4 * 3.14 * radius * radius;
+result = 4 * Pi * radius * radius;
 
  std::cout << "result: " << result << '\n';
 
--- percentage.cpp
+++ percentage.cpp
@@ -13,6 +13,7 @@ double result;
     std::cout <<"\n***************Percentage Calculator***************\n";
     std::cout <<"Enter the number you would like to find the percentage of: ";
     std::cin >> num1;
+    
     std::cout <<"What percent of the number would you like to find?\nPlease enter the what percent you would like: ";
     std::cin >> num2;
 
