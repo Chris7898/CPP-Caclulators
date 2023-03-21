c658a2716fe184820b003ea6dcd2969a07b6ceb0
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-21 14:34:17 -0400

    Circle Perimeter calc

---
+++ CirclePerimeter.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+double radius;
+double diameter;
+double result;
+const double pi = 3.14;
+char op;
+
+int main(){
+
+    std::cout << "\n********************Circle Perimeter calc********************\n";
+    
+    std::cout << "please choose either radius or diameter\n";
+    std::cout << "Use R for radius or D for diameter.\n";
+    std::cout << "Enter R or D: ";
+    std::cin >> op;
+
+switch (op)
+{
+case 'R':
+    std::cout <<"Enter your Radius: ";
+    std::cin >> radius;
+    result = 2 * pi * radius;
+    std::cout <<"result: " << result;
+
+    break;
+
+case 'D':
+    std::cout <<"Enter your Diameter: ";
+    std::cin >> radius;
+    result = pi * diameter;
+    std::cout <<"Result: " << result;
+
+    break;
+
+default:
+std::cout <<"Invlaid!";
+    break;
+
+}
+
+
+return 0;
+}
