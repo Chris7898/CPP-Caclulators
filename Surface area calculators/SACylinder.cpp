59aa545f624965e6dd31e2020ffacb58db65ac88
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-23 14:10:07 -0400

    [complete] Did all the math for SACylinder.cpp

--- Surface area calculators/SACylinder.cpp
+++ Surface area calculators/SACylinder.cpp
@@ -3,9 +3,23 @@
 #include <iostream>
 #include <cmath>
 
+const double pi = 3.14;
+double result;
+double height;
+double radius;
+
+
 int main(){
 
+    std::cout <<"Enter the height: ";
+    std::cin >> height;
 
+    std::cout <<"Enter the radius: ";
+    std::cin >> radius;
+
+    result = (2 * pi * radius * height) + ( 2 * pi * pow(radius, 2));
+
+    std::cout <<"Result:" << result <<'\n';
 
     return 0;
 }
