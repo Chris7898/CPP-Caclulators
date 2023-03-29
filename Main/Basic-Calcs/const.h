94c4292d51b2bcf711b5b456b57e2664323add0e
Darkwolf7898 <115883974+Darkwolf7898@users.noreply.github.com>
2023-03-29 14:38:22 -0400

    fixed calc.cpp to use const. .exe file made

--- Main/Basic-Calcs/const.h
+++ Main/Basic-Calcs/const.h
@@ -3,6 +3,10 @@
 // Used multiple times
 const double pi = 3.14;
 double result;
+double radius;
+double a;
+double b;
+double c;
 
 // SASquarePyramid.cpp
 double BaseEdge;
@@ -11,31 +15,19 @@ double Slant;
 double BasePerimeter;
 
 // SASphere.cpp
-double radius;
 
 // SaCylinder.cpp
 double height;
-double radius;
 
 // SACube.cpp
 double num;
 
-// SACircle
-double radius;
-
 // TrianglePerimeter.cpp
-double a;
-double b;
-double c;
 
 // SquarePerimeter.cpp
-double a;
-double b;
-double c;
 double d;
 
 // CirclePerimeter
-double radius;
 double diameter;
 
 // Basic calc and percentage
