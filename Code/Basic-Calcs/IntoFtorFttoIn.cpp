eeff88dfd6ab12ed70e2cee3aa7cf80aedfe39a9
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2023-06-19 03:32:40 -0400

    intoft

---
+++ Code/Basic-Calcs/IntoFtorFttoIn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int input;
+double inches;
+double feet;
+double result;
+
+int main(){
+    std::cout <<"Hello! please select a valid option.\n";
+    std::cout <<"1: Feet to Inches \n2: Inches to Feet \n";
+    std::cin >> input;
+
+    
+
+}
